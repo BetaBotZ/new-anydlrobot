7b1661ef66c42740107285cc6d9ef115d436c721
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-03-29 21:21:26 +0530

    Update server_details.py

--- plugins/server_details.py
+++ plugins/server_details.py
@@ -4,23 +4,6 @@ import psutil
 import time
 bot_start_time = time.time()
 
-''' def get_readable_time(seconds: int) -> str:
-    result = ''
-    (days, remainder) = divmod(seconds, 86400)
-    days = int(days)
-    if days != 0:
-        result += f'{days}d'
-    (hours, remainder) = divmod(remainder, 3600)
-    hours = int(hours)
-    if hours != 0:
-        result += f'{hours}h'
-    (minutes, seconds) = divmod(remainder, 60)
-    minutes = int(minutes)
-    if minutes != 0:
-        result += f'{minutes}m'
-    seconds = int(seconds)
-    result += f'{seconds}s'
-    return result '''
 
 @Client.on_message(filters.command(["server"]), group=-2)
 async def start(client, message):
