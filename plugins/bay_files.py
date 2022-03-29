ecbfca2b7e090bc9f156a2d7ec4a2b672babeada
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-03-29 21:10:57 +0530

    Update bay_files.py

--- plugins/bay_files.py
+++ plugins/bay_files.py
@@ -34,7 +34,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["bay"]))
 async def get_link(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
+    if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
             message_ids=update.message_id,
