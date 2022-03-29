4018e8d925cc8295617c5cf6fae0ff4e31b4d2e0
Adarsh Goel <88939380+adarsh-goel@users.noreply.github.com>
2022-03-29 21:21:46 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -3,23 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import time
 bot_start_time = time.time()
 
-''' def get_readable_time(seconds: int) -> str:
-    result = ''
-    (days, remainder) = divmod(seconds, 86400)
-    days = int(days)
-    if days != 0:
-        result += f'{days}d'
-    (hours, remainder) = divmod(remainder, 3600)
-    hours = int(hours)
-    if hours != 0:
-        result += f'{hours}h'
-    (minutes, seconds) = divmod(remainder, 60)
-    minutes = int(minutes)
-    if minutes != 0:
-        result += f'{minutes}m'
-    seconds = int(seconds)
-    result += f'{seconds}s'
-    return result '''
+
 
 @Client.on_message(filters.command(["start"]), group=-2)
 async def start(client, message):
