0e224c35c6b8aa20fda0c2b264ed176728b36314
༺✦҈͜͡➳ιgиιтє∂ мιи∂ѕ࿐༻ <91360737+OO7ROBot@users.noreply.github.com>
2022-05-03 08:53:13 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -9,9 +9,9 @@ bot_start_time = time.time()
 async def start(client, message):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("JOIN", url="https://t.me/agprojects")],
+        [InlineKeyboardButton("JOIN", url="https://t.me/MyTestBotZ")],
         [InlineKeyboardButton(
-            "Owner", url="https://t.me/adarsh_goel")]
+            "Owner", url="https://t.me/OO7ROBOT")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
